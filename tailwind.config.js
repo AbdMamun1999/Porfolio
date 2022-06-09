57083e6d796fad76b6889d844db6f679b5de8599
AbdMamun1999 <mamunrox@gmail.com>
2022-06-09 12:19:20 +0600

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        manufacturertheme: {
+          primary: "#1d293e",
+          secondary: "#62c0ec",
+          accent: "#62c0ec",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      'cupcake'
+    ],
+  },
+  plugins: [require("daisyui")],
+}
